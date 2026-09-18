81faba3a6ef1c1d9b3700d4d66723fb80e09c3f2
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-19 05:19:50 +0700

    #5 Todo: -Добавление обработчиков событий для компонентов.

--- src/components/SearchTaskForm/SearchTaskForm.jsx
+++ src/components/SearchTaskForm/SearchTaskForm.jsx
@@ -1,15 +1,21 @@
 import Field from '../Field'
 
 const SearchTaskForm = (props) => {
-  const {} = props
+  const {
+    onSearchInput,
+  } = props
 
   return (
-    <form className="todo__form">
+    <form
+      className="todo__form"
+      onSubmit={(event) => event.preventDefault()}
+    >
       <Field
         className="todo__field"
         label="Search task"
         id="search-task"
         type="search"
+        onInput={(event) => onSearchInput(event.target.value)}
       />
     </form>
   )
