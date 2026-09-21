1662d3609ccb92f1eedf6781e51602ac50c70de3
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-21 13:51:56 +0700

    #16 Todo: -Анимации добавления и удаления задач из списка

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -5,6 +5,8 @@ const useTasks = () => {
   const [tasks, setTasks] = useState([])
   const [newTaskTitle, setNewTaskTitle] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
+  const [disappearingTaskId, setDisappearingTaskId] = useState(null)
+  const [appearingTaskId, setAppearingTaskId] = useState(null)
 
   const newTaskInputRef = useRef(null)
 
@@ -20,9 +22,13 @@ const useTasks = () => {
   const deleteTask = useCallback((taskId) => {
     tasksAPI.delete(taskId)
       .then(() => {
-        setTasks(
-          tasks.filter((task) => task.id !== taskId)
-        )
+        setDisappearingTaskId(taskId)
+        setTimeout(() => {
+          setTasks(
+            tasks.filter((task) => task.id !== taskId)
+          )
+          setDisappearingTaskId(null)
+        }, 400)
       })
   }, [tasks])
 
@@ -53,6 +59,10 @@ const useTasks = () => {
         setNewTaskTitle('')
         setSearchQuery('')
         newTaskInputRef.current.focus()
+        setAppearingTaskId(addedTask.id)
+        setTimeout(() => {
+          setAppearingTaskId(null)
+        }, 400)
       })
   }, [])
 
@@ -79,7 +89,9 @@ const useTasks = () => {
     searchQuery,
     setSearchQuery,
     newTaskInputRef,
-    addTask
+    addTask,
+    disappearingTaskId,
+    appearingTaskId,
   }
 }
 
